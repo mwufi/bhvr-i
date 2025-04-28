16cd4c65835e3dee43fb391820145b74364b1b6f
Zen T <ztang230@gmail.com>
2025-04-28 17:08:14 -0500

    add tailwind and client side instantdb

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,63 +1,176 @@
-import { useState } from 'react'
-import beaver from './assets/beaver.svg'
-import { ApiResponse } from 'shared'
-import './App.css'
+import { id, i, init, InstaQLEntity } from "@instantdb/react";
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000"
+// ID for app: personalDash
+// Replace with your own ID!
+const APP_ID = "19a9cbb9-2c1b-4591-a5f5-498e93c5803d";
+
+// Optional: Declare your schema!
+const schema = i.schema({
+  entities: {
+    todos: i.entity({
+      text: i.string(),
+      done: i.boolean(),
+      createdAt: i.number(),
+    }),
+  },
+});
+
+type Todo = InstaQLEntity<typeof schema, "todos">;
+
+const db = init({ appId: APP_ID, schema });
 
 function App() {
-  const [data, setData] = useState<ApiResponse | undefined>()
-
-  async function sendRequest() {
-    try {
-      const req = await fetch(`${SERVER_URL}/hello`)
-      const res: ApiResponse = await req.json()
-      setData(res)
-    } catch (error) {
-      console.log(error)
-    }
+  // Read Data
+  const { isLoading, error, data } = db.useQuery({ todos: {} });
+  if (isLoading) {
+    return;
   }
-
+  if (error) {
+    return <div className="text-red-500 p-4">Error: {error.message}</div>;
+  }
+  const { todos } = data;
   return (
-    <>
-      <div>
-        <a href="https://github.com/stevedylandev/bhvr" target="_blank">
-          <img src={beaver} className="logo" alt="beaver logo" />
-        </a>
+    <div className="font-mono min-h-screen flex justify-center items-center flex-col space-y-4">
+      <h2 className="tracking-wide text-5xl text-gray-300">todos</h2>
+      <div className="border border-gray-300 max-w-xs w-full">
+        <TodoForm todos={todos} />
+        <TodoList todos={todos} />
+        <ActionBar todos={todos} />
       </div>
-      <h1>bhvr</h1>
-      <h2>Bun + Hono + Vite + React</h2>
-      <p>A typesafe fullstack monorepo</p>
-      <div className="card">
-        <button onClick={sendRequest}>
-          Call API
-        </button>
-        {data && (
-          <pre className='response'>
-            <code>
-            Message: {data.message} <br />
-            Success: {data.success.toString()}
-            </code>
-          </pre>
-        )}
-        <pre className='code'>
-          <code>
-{`
-  .
-  ├── client/               # React frontend
-  ├── server/               # Hono backend
-  ├── shared/               # Shared TypeScript definitions
-  │   └── src/types/        # Type definitions used by both client and server
-  └── package.json          # Root package.json with workspaces
-`}
-          </code>
-        </pre>
+      <div className="text-xs text-center">
+        Open another tab to see todos update in realtime!
       </div>
-      <p className="read-the-docs">
-        Click the beaver to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+// Write Data
+// ---------
+function addTodo(text: string) {
+  db.transact(
+    db.tx.todos[id()].update({
+      text,
+      done: false,
+      createdAt: Date.now(),
+    })
+  );
+}
+
+function deleteTodo(todo: Todo) {
+  db.transact(db.tx.todos[todo.id].delete());
+}
+
+function toggleDone(todo: Todo) {
+  db.transact(db.tx.todos[todo.id].update({ done: !todo.done }));
+}
+
+function deleteCompleted(todos: Todo[]) {
+  const completed = todos.filter((todo) => todo.done);
+  const txs = completed.map((todo) => db.tx.todos[todo.id].delete());
+  db.transact(txs);
+}
+
+function toggleAll(todos: Todo[]) {
+  const newVal = !todos.every((todo) => todo.done);
+  db.transact(
+    todos.map((todo) => db.tx.todos[todo.id].update({ done: newVal }))
+  );
+}
+
+
+// Components
+// ----------
+function ChevronDownIcon() {
+  return (
+    <svg viewBox="0 0 20 20">
+      <path
+        d="M5 8 L10 13 L15 8"
+        stroke="currentColor"
+        fill="none"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
+
+function TodoForm({ todos }: { todos: Todo[] }) {
+  return (
+    <div className="flex items-center h-10 border-b border-gray-300">
+      <button
+        className="h-full px-2 border-r border-gray-300 flex items-center justify-center"
+        onClick={() => toggleAll(todos)}
+      >
+        <div className="w-5 h-5">
+          <ChevronDownIcon />
+        </div>
+      </button>
+      <form
+        className="flex-1 h-full"
+        onSubmit={(e) => {
+          e.preventDefault();
+          const input = e.currentTarget.input as HTMLInputElement;
+          addTodo(input.value);
+          input.value = "";
+        }}
+      >
+        <input
+          className="w-full h-full px-2 outline-none bg-transparent"
+          autoFocus
+          placeholder="What needs to be done?"
+          type="text"
+          name="input"
+        />
+      </form>
+    </div>
+  );
+}
+
+function TodoList({ todos }: { todos: Todo[] }) {
+  return (
+    <div className="divide-y divide-gray-300">
+      {todos.map((todo) => (
+        <div key={todo.id} className="flex items-center h-10">
+          <div className="h-full px-2 flex items-center justify-center">
+            <div className="w-5 h-5 flex items-center justify-center">
+              <input
+                type="checkbox"
+                className="cursor-pointer"
+                checked={todo.done}
+                onChange={() => toggleDone(todo)}
+              />
+            </div>
+          </div>
+          <div className="flex-1 px-2 overflow-hidden flex items-center">
+            {todo.done ? (
+              <span className="line-through">{todo.text}</span>
+            ) : (
+              <span>{todo.text}</span>
+            )}
+          </div>
+          <button
+            className="h-full px-2 flex items-center justify-center text-gray-300 hover:text-gray-500"
+            onClick={() => deleteTodo(todo)}
+          >
+            X
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function ActionBar({ todos }: { todos: Todo[] }) {
+  return (
+    <div className="flex justify-between items-center h-10 px-2 text-xs border-t border-gray-300">
+      <div>Remaining todos: {todos.filter((todo) => !todo.done).length}</div>
+      <button
+        className=" text-gray-300 hover:text-gray-500"
+        onClick={() => deleteCompleted(todos)}
+      >
+        Delete Completed
+      </button>
+    </div>
+  );
+}
+
+export default App;
